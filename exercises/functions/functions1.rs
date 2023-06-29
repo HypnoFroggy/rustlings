ccf5b2651faa55b88c4906e07236747e9befc512
Mitya <dmitrij.mal.tim.04@gmail.com>
2023-06-29 23:00:42 +0300

    first commit with all the shit

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,13 @@
+// functions1.rs
+//
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
+// hint.
+
+fn call_me(){
+    println!("Call me, call me");
+    println!("Say that you call me");
+}
+
+fn main() {
+    call_me();
+}
